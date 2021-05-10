aeaf60ed5540718dba88e94f898f65cb0b11e217
Ilya Rozhok <i_rozhok@outlook.com>
2021-05-10 20:29:58 +0300

    Updated prev commit with carousel

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right.png"></button>',
+        adaptiveHeight: true,
+        speed: 1200
+      });
+  });
