82bbe1f07902e4abda019be9b7c5a78895d80dda
Developer Ilya <ilyadevelop@IlyaRozhok.local>
2021-05-18 00:24:47 +0300

    Added tiny-slider cdn

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"></button>',
         adaptiveHeight: true,
         speed: 1200
       });
+  }); */
+  const slider = tns({
+    container: '.tiny-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controlsText: [
+      '<img src="icons/chevron_left.png">',
+      '<img src="icons/chevron_right.png">'
+    ]
   });
